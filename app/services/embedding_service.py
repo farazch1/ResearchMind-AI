c3f2eff989c9ded31e528b230d709fa7432d8590
Mian Faraz <mianfaraz2002@gmail.com>
2026-07-26 01:02:21 +0500

    change a little

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,11 +1,14 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-base-en-v1.5",
-    model_kwargs={"device": "cpu"},
-    encode_kwargs={"normalize_embeddings": True},
-)
+_embedding_model = None
 
 
 def get_embedding_model():
-    return embedding_model
+    global _embedding_model
+    if _embedding_model is None:
+        _embedding_model = HuggingFaceEmbeddings(
+            model_name="BAAI/bge-base-en-v1.5",
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={"normalize_embeddings": True},
+        )
+    return _embedding_model
