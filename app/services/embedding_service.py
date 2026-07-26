1b2eed6b3c029ff52a81e5a411206d82913eb22a
Mian Faraz <mianfaraz2002@gmail.com>
2026-07-26 21:11:14 +0500

    done

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,9 +1,9 @@
 import os
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
-
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 
 def get_embedding_model():
-    return HuggingFaceInferenceAPIEmbeddings(
-        api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
-        model_name="BAAI/bge-small-en-v1.5",
+    return HuggingFaceEndpointEmbeddings(
+        model="BAAI/bge-small-en-v1.5",
+        task="feature-extraction",
+        huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
     )
